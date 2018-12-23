dc68cbeeb369c358d2873806174efed1e6abb8b8
kunalk <kunalkhanvilkar.pune@gmail.com>
2018-12-23 13:19:58 +0530

    create proj files

---
+++ src/main/java/io/kunalk/springaws/dynamoDBweb/DynamoDBwebApplication.java
@@ -0,0 +1,14 @@
+package io.kunalk.springaws.dynamoDBweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDBwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDBwebApplication.class, args);
+	}
+
+}
+
